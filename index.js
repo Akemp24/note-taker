ba7b447d3aa300e1089ad4f2e3f19714741530f7
Asiak <asiakemp24@gmail.com>
2023-07-02 12:12:09 -0400

    wrote down required, created instance of express, set up middleware

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // write down required 
+const express = require('express');
+const path = require('path');
 
 // create instance of express
+const app = express();
 
 // Set up middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // define routes to handle requests
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
 // create html route for index.html
 
 // create html route for notes.html
