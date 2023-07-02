399bd6f2f22b68db07282c60546ad405e05df613
Asiak <asiakemp24@gmail.com>
2023-07-02 11:56:47 -0400

    pseudo-coded what I neeeded

---
+++ index.js
@@ -0,0 +1,17 @@
+// write down required 
+
+// create instance of express
+
+// Set up middleware
+
+// define routes to handle requests
+
+// create html route for index.html
+
+// create html route for notes.html
+
+// create api route to get all notes from data from db.json and return as JSON
+
+// create api to save a new note
+
+// connect back end to front end
